df071a7d14946891db60e08c685cfa12e9254be6
niuyun <461104031@qq.com>
2020-05-09 14:10:53 +0800

    创建主入口文件和css文件夹

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-09 13:49:24
+ * @LastEditTime: 2020-05-09 13:49:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vs\index.js
+ */
