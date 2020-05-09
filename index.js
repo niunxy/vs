66d59d4395b4d23eb1acf32c3ce09e0148da671e
niuyun <461104031@qq.com>
2020-05-09 14:47:07 +0800

    升级到1.1版本

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2020-05-09 13:49:24
- * @LastEditTime: 2020-05-09 14:41:29
+ * @LastEditTime: 2020-05-09 14:46:20
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vs\index.js
  */
-let version = 1.0
+let version = 1.1
