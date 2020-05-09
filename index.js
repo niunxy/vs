1efd70336ea9f2976acf59149a3f9164d14e9a66
niuyun <461104031@qq.com>
2020-05-09 14:42:09 +0800

    添加version变量

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2020-05-09 13:49:24
- * @LastEditTime: 2020-05-09 13:49:25
+ * @LastEditTime: 2020-05-09 14:41:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vs\index.js
  */
+let version = 1.0
